6323c90b73a8fd438ef059d9c605e96139f690aa
Praveenkodadala <55790663+Praveenkodadala@users.noreply.github.com>
2022-07-22 11:00:45 +0530

    modified binary search

--- binarySearch.js
+++ binarySearch.js
@@ -4,15 +4,12 @@ var binarySearch = (array, elem) => {
   let middle = Math.floor((start + end) / 2);
   //console.log(start, middle, end);
   while (array[middle] !== elem && start <= end) {
-    if (elem < array[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
+    if (elem < array[middle]) end = middle - 1;
+     else start = middle + 1;
     middle = Math.floor((start + end) / 2);
   }
   console.log(start, middle, end);
   if (array[middle] == elem) return middle;
     return -1;
 };
-console.log(binarySearch([2, 4, 5, 8, 10, 12, 15, 18], 10));
+console.log(binarySearch([2, 4, 5, 8, 10, 12, 15, 18], 15));
